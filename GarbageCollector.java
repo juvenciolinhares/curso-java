4943604dd41d3e29b269131ad558a78ae68fd775
Juvencio <jlinhares@cpqi.com>
2022-06-08 12:07:01 -0300

    garbage collector

--- GarbageCollector.java
+++ GarbageCollector.java
@@ -2,8 +2,41 @@
 public class GarbageCollector {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * variavel primitiva 
+		 * quando o valor é atualizado, o valor antigo é substituido.
+		 */
+		int idade;
+		double peso; 
+		boolean casado;
+		
+		/*variavel de referencia:
+		 * toda variavel que aponta para um objeto
+		 */
+		String nome; 
+		/*
+		 * garbage colector atua nas variaveis de referencia
+		 * quando esses valores são alterados para um novo valor
+		 * o valor antigo ainda permanece na memória
+		 * exemplo:
+		 */
+		nome = "Juvencio";
+		System.out.println(nome);
+		
+		nome = "Ana Thereza";
+		System.out.println(nome);
+		/*
+		 * o nome juvencio ainda fica alocado em memoria
+		 * então o garbage collector tem a função de exlcuir esse valor 
+		 * que nao faz mais referencia a nenhuma variavel.
+		 */
+		
+		nome = null;
+		System.out.println(nome);
+		/*
+		 * a variavel nao esta mais apontando pra nada 
+		 * nesse caso Ana Thereza também vai ser exlcuido pelo Garbage collector
+		 */
 	}
 
 }
