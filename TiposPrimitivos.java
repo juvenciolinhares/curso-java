4ab008d799dfa0802c34c0a28efa4b7837a65bc5
Juvencio <jlinhares@cpqi.com>
2022-06-08 11:17:21 -0300

    adicionados tipos primitos e variaveis

---
+++ TiposPrimitivos.java
@@ -0,0 +1,32 @@
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+
+		/*tipos primitivos:
+		 * int, byte, short,long(numero inteiros)
+		 * float, double(ponto flutuante)
+		 * char void boolean
+		 * alocados na memória stack, são apenas um valor,
+		 * não são objetos.
+		 */
+		char sexo = 'F';
+		byte b =127; 
+		short s = 32760;
+		int i = 2000000000; 
+		long l =900000000;
+		double peso = 56.43;
+		float temperatura= 36.5f;
+
+		//formato binario:
+		byte bb = 0b01010101;//System.out.println(bb);=> 85
+		short ss = 0b0101010101010101;//System.out.println(ss);=> 21845
+		int ii = 0b01010101010101010101010101010101;	//System.out.println(ii);=> 1431655765
+
+		//cast implicito: uma variavel menor "cabe numa maior"
+		i = s;
+		//System.out.println(i); => 32760
+		//cast explicito=> quando assumo a perda de informações:
+		i = (int)peso;
+		//System.out.println(i);=> 56, pedir os valores decimais. 
+	}
+}
