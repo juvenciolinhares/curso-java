10e6ab9f4e59115ca3918feb4806704ec731e940
Juvencio <jlinhares@cpqi.com>
2022-06-08 17:41:57 -0300

    Operadores De Atribuicao

---
+++ OperadoresDeAtribuicao.java
@@ -0,0 +1,26 @@
+
+public class OperadoresDeAtribuicao {
+
+	public static void main(String[] args) {
+		
+		int x = 6;
+		System.out.println(x);
+		
+		 x += 3;
+		 System.out.println(x);
+		 
+		 x -= 3;
+		 System.out.println(x);
+		 
+		 x *= 3;
+		 System.out.println(x);
+		 
+		 x /= 3;
+		 System.out.println(x);
+		 
+		 x %= 3;
+		 System.out.println(x);
+
+	}
+
+}
