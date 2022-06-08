bd1a16a254f990cb1bb5abb25d73db0e0c44d192
Juvencio <jlinhares@cpqi.com>
2022-06-08 17:36:07 -0300

    Operadores Logicos

---
+++ OperadoresLogicos.java
@@ -0,0 +1,19 @@
+
+public class OperadoresLogicos {
+	public static void main (String [] args) {
+		
+		//operadores lógicos retornam um booleano
+		
+		//utilizando && and(e):
+		int x = 6;
+		System.out.println((x >= 1) && (x <= 10));
+		
+		//utilizando || or(ou):
+		System.out.println((x >= 1) || (x <= 5));
+		
+		//utilizando ! not(negação):
+		
+		System.out.println(!(x >= 1));
+	}
+
+}
